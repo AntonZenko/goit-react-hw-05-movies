09c1cd90713e60fb2f3ce00084d1c32b25c53237
AntonZenko <94699930+AntonZenko@users.noreply.github.com>
2022-09-07 10:11:56 +0000

    Deploying to gh-pages from @ AntonZenko/goit-react-hw-05-movies@80b6fb287ddadf73da917d7ea4a4634f7d8520c2 🚀

---
+++ static/js/980.ac424881.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[980],{9980:function(e,n,t){t.r(n);var c=t(501),i=t(184);n.default=function(){return(0,i.jsxs)("div",{children:[(0,i.jsx)("h2",{children:"Page not found"}),(0,i.jsx)(c.rU,{to:"/",children:"Go back"})]})}}}]);
+//# sourceMappingURL=980.ac424881.chunk.js.map
